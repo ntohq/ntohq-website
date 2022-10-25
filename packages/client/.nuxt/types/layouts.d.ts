a83126137492e204e0f9ffd6eb771feff2656f3f
Wesley Ford <wes@wesdev.pro>
2022-10-25 15:37:24 -0500

    Attempt to FIX Layouts

--- packages/client/.nuxt/types/layouts.d.ts
+++ packages/client/.nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
+export type LayoutKey = "default" | "private"
 declare module "F:/GitHub/ntohq-website/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
