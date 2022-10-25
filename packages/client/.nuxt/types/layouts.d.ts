9e5593b7930d2fec677535c8e5f7dea7f2558e53
Wesley Ford <wes@wesdev.pro>
2022-10-25 15:45:21 -0500

    Add Layouts

--- packages/client/.nuxt/types/layouts.d.ts
+++ packages/client/.nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "private"
+export type LayoutKey = "default"
 declare module "F:/GitHub/ntohq-website/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
